3011e422dce093037e9d8942415000cb2daa40fd
Nathane Almeida <nathane.almeida@g.globo>
2021-08-31 19:52:37 -0300

    exc003

---
+++ exercise/exc003.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int = 26
+var y string = "Nathane"
+var z bool = true
+
+func main() {
+
+	fmt.Printf("%v %T \n", x, x)
+	fmt.Printf("%v %T \n", y, y)
+	fmt.Printf("%v %T \n", z, z)
+}
